f501b75db84a6c48e114d626d8077fbd26d73c4b
KyBrMar12 <KyBrMar@gmail.com>
2025-01-21 22:32:11 -0600

    Final push for images to appear hopefully

--- react-portfolio/src/components/AboutMe.jsx
+++ react-portfolio/src/components/AboutMe.jsx
@@ -4,7 +4,7 @@ import '../styles/AboutMe.css';
 const AboutMe = () => (
   <section>
     <h2>About Me</h2>
-    <img src="src/assets/images/profile-pic.jpg" alt="Kyle Marcak" />
+    <img src="/images/profile-pic.jpg" alt="Kyle Marcak" />
     <p>Born and raised in Texas, I had a passion about space travel as a kid. My father was a software developer for Compaq at the time. Later it would be known as HP. This had a profound effect on me as I wanted to become an engineer as well, just like my dad. </p>
     <br></br>
     <p>Once I saw the Iron Man Movie in 2008 I wanted to specifically be a mechanical engineer. This was a dream of mine until I encountered Thermodynamics. I had to make a choice half way through my collegiate career to either stay in engineering or to switch. I chose the later, and switched to Finance at the suggestion of my grandpa, as I was able to retain alot of enjoyable skills that transfered over into the field. </p>
--- react-portfolio/src/components/Portfolio.jsx
+++ react-portfolio/src/components/Portfolio.jsx
@@ -6,37 +6,37 @@ const Portfolio = () => {
   const projects = [
     {
       title: 'Mailbox rental application',
-      image: 'src/assets/images/Medium mailboxes.png',
+      image: '/images/Medium mailboxes.png',
       deployedLink: 'https://kybrmar12.github.io/Team-Ship-N-Go/',
       repoLink: 'https://github.com/KyBrMar12/Team-Ship-N-Go.git',
     },
     {
       title: 'Vehicle Builder',
-      image: '/src/assets/images/toyota tundra photo.avif',
+      image: '/images/toyota tundra photo.avif',
       deployedLink: 'https://drive.google.com/file/d/12qbA4ETUGOh14LsDdgoOSGStazdA3Ssn/view',
       repoLink: 'https://github.com/KyBrMar12/Amazing-Vehicle-Builder-Kyle-Style.git',
     },
     {
       title: 'Employee Tracker',
-      image: '/src/assets/images/office space photo.jpg',
+      image: '/images/office space photo.jpg',
       deployedLink: 'https://drive.google.com/file/d/1WbTs-4VQaHU2jjkq2VhXJD8-lvuxwVVb/view',
       repoLink: 'https://github.com/KyBrMar12/Employee-Tracker-Super-Kyle-Style.git',
     },
     {
       title: 'README.md Generator',
-      image: '/src/assets/images/nodejs photo.png',
+      image: '/images/nodejs photo.png',
       deployedLink: 'https://drive.google.com/file/d/12s1NGh0PzDYathmIDTokQUg58NTwh7dU/view',
       repoLink: 'https://github.com/KyBrMar12/Generated-README-Kyle-Style.git',
     },
     {
       title: 'Weather Dashboard',
-      image: 'src/assets/images/earth photo.jpg',
+      image: '/images/earth photo.jpg',
       deployedLink: 'https://deployed-project5.com',
       repoLink: 'https://github.com/KyBrMar12/Weather-Dashboard-Kyle-Style.git',
     },
     {
       title: 'React Portfolio',
-      image: 'src/assets/images/react js photo.jpg',
+      image: '/images/react js photo.jpg',
       deployedLink: 'https://deployed-project6.com',
       repoLink: 'https://github.com/KyBrMar12/React-Portfolio-Kyle-Style.git',
     },
