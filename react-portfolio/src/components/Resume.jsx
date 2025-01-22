9b7caefcd409b1431919ecb3d4640d5943666c98
KyBrMar12 <KyBrMar@gmail.com>
2025-01-21 21:11:46 -0600

    My 2025 Resume is now downloadable from clicking the link

---
+++ react-portfolio/src/components/Resume.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../styles/Resume.css';
+
+const Resume = () => (
+  <section>
+    <h2>Resume</h2>
+    <a href="src/assets/Kyle Marcak's Resume 2025.pdf" download>Download Resume</a>
+    <h3>Proficiencies</h3>
+    <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+      <li>React</li>
+      <li>Node.js</li>
+    </ul>
+  </section>
+);
+
+export default Resume;
