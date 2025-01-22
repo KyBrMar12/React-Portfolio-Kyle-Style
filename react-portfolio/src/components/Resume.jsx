9bfd22dd07e9323e15b1af85df4f6b54302743db
KyBrMar12 <KyBrMar@gmail.com>
2025-01-22 17:18:21 -0600

    Forgot to make my Resume public for netlify deployment, finally de-bugged and pushing changes

--- react-portfolio/src/components/Resume.jsx
+++ react-portfolio/src/components/Resume.jsx
@@ -4,7 +4,7 @@ import '../styles/Resume.css';
 const Resume = () => (
   <section>
     <h2>Resume</h2>
-    <a href="src/assets/Kyle Marcak's Resume 2025.pdf" download>Download Resume</a>
+    <a href="/Kyle Marcak's Resume 2025.pdf" download>Download Resume</a>
     <h3>Proficiencies</h3>
     <ul>
       <li>HTML</li>
