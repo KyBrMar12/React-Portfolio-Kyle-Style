138af80a8915d3852a6c5493cf69f8be3174f95b
KyBrMar12 <KyBrMar@gmail.com>
2025-01-21 19:19:02 -0600

    uploading the header and footer contents for the portfolio

---
+++ react-portfolio/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Footer.css';
+
+const Footer = () => (
+  <footer>
+    <a href="https://github.com/KyBrMar12" target="_blank" rel="noopener noreferrer">GitHub</a>
+    <a href="https://www.linkedin.com/in/kyle-marcak-788149151/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+    <a href="https://stackoverflow.com/users/29305817/kybrmar12" target="_blank" rel="noopener noreferrer">Stack Overflow</a>
+  </footer>
+);
+
+export default Footer;
---
+++ react-portfolio/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navigation from './Navigation';
+import '../styles/Header.css';
+
+const Header = () => (
+  <header>
+    <h1>Kyle Marcak</h1>
+    <Navigation />
+  </header>
+);
+
+export default Header;
